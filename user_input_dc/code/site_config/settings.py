391ea01bb03367c75ed84323214d7c9c88075b3f
Greg-Hawkridge <ghawkridge@gmail.com>
2023-01-30 16:32:51 +0000

    Remove django secret key from public source code

--- user_input_dc/code/site_config/settings.py
+++ user_input_dc/code/site_config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ljn3_(^ngxvwsrpw_7)uzaux@d5xvrwil_r1$5sg$c(vw16e&p'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
